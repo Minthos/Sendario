2164ac930e8221de928de61f41d0e6733667808c
Espen Overaae <minthos@gmail.com>
2023-07-14 23:35:01 +0200

    I have an idea..

--- Source/subparcollider/render5.cpp
+++ Source/subparcollider/render5.cpp
@@ -386,9 +386,12 @@ Boxoid exampleBoxoids[2] =
 	4, 0x0}
 };
 
+float lerp(float a, float b, float f) {
+	return (b * f) + (a * (1.0f - f));
+}
 
 glm::vec3 vlerp(glm::vec3 a, glm::vec3 b, float f) {
-	return (a * f) + (b * (1.0f - f));
+	return (b * f) + (a * (1.0f - f));
 }
 
 GLuint VERT_ORIGINAL = 1;
@@ -498,10 +501,10 @@ Vertex vertex_interpolate(Vertex* a, Vertex* b, float c) {
 	return Vertex(vlerp(a->position, b->position, c), glm::normalize(vlerp(a->normal, b->normal, c)), vlerp(a->light, b->light, c), a->flags & b->flags);
 }
 
-// Q is origin, P0 is a point on the plane, N is the plane normal. Returns the vector to the nearest point on the plane from the origin
-glm::vec3 nearestPointOnPlane(glm::vec3 Q, glm::vec3 P0, glm::vec3 N) {
-    return P0 + glm::dot(Q - P0, N) * N;
+glm::vec3 nearestPointOnPlane(glm::vec3 origin, glm::vec3 onPlane, glm::vec3 normal) {
+    return onPlane + glm::dot(origin - onPlane, normal) * normal;
 }
+
 // Loop subdivision that doesn't create duplicate edges/vertices, runs in linear time
 Mesh tessellateMesh(Mesh* original, int iteration, Boxoid* box) {
 	GLuint numVerts = original->numEdges + original->numVerts;
@@ -525,9 +528,6 @@ Mesh tessellateMesh(Mesh* original, int iteration, Boxoid* box) {
 		verts[vertIndex] = vertex_interpolate(&verts[edge->verts[0]], &verts[edge->verts[1]], 0.5f);
 		verts[vertIndex].flags &= ~VERT_ORIGINAL;
 		edge->verts[2] = vertIndex;
-		if(iteration == 0) { // the light value isn't being used for light yet so we'll use it for this instead
-			verts[vertIndex].light.x = 1.0f;
-		}
 		edges[i * 2] = Edge(edge->verts[0], edge->verts[2]);
 		edges[i * 2 + 1] = Edge(edge->verts[1], edge->verts[2]);
 		edge->subdivisions[0] = &edges[i * 2];
@@ -574,19 +574,32 @@ Mesh tessellateMesh(Mesh* original, int iteration, Boxoid* box) {
 			//printf("indices: %d %d %d\n", indices[i * 9 + j * 3], indices[i * 9 + j * 3 + 1], indices[i * 9 + j * 3 + 2]);
 		}
 		for(int j = 0; j < 3; j++) {
-			// should discriminate between these two but we'll just add them together for now
-			float curvature = box->curvature[tri->faceIndex * 2] + box->curvature[tri->faceIndex * 2 + 1];
-			// light is a misnomer. this is heavy. it's also repurposing the "light" variable to store temporary values for the curvature calculation, which is the whole reason we are tessellating this mesh in the first place
-			glm::vec3 light = verts[newVertices[j]].light;
+			Vertex* v = &verts[newVertices[j]];
+			glm::vec3 light = v->light;
+			GLuint fi = tri->faceIndex;
+			float ownDistance = glm::length(v->position - original->centre);
+			// this is correct. The resulting shape is a smooth spheroid.
+			glm::vec3 onSpheroid = v->position * (light.y / ownDistance);
+		
+			// this is not correct. the shape defined by this function is not a box.	
+			glm::vec3 onBox = nearestPointOnPlane(v->position, original->faceCentres[fi], original->faceNormals[fi]);
+			
+			float curvature = box->curvature[fi * 2] + box->curvature[fi * 2 + 1];
+			//float unitcurve = sqrt(1.0f - (1.0f - light.x) * (1.0f - light.x));
+			//glm::vec3 fromCentre = v->position - original->centre;
+			//float distance = glm::length(fromCentre);
+			//float faceDistanceToCentre = glm::length(onFace - original->centre);
+			//float target = lerp(light.y, faceDistanceToCentre, curvature * 0.25f);
+			//float target = faceDistanceToCentre;
+			//float target = light.y;
+			//float adjustment = target - distance;
+			//glm::vec3 offset = verts[newVertices[j]].normal * adjustment;
 
-			float unitcurve = sqrt(1.0f - (1.0f - light.x) * (1.0f - light.x));
-			float target = light.y;
-			glm::vec3 fromCentre = verts[newVertices[j]].position - original->centre;
-			float distance = glm::length(fromCentre);
-			float adjustment = target - distance;
-			glm::vec3 offset = verts[newVertices[j]].normal * adjustment;
-			verts[newVertices[j]].position += offset;
+			//verts[newVertices[j]].position += offset;
+			verts[newVertices[j]].position = vlerp(onBox, onSpheroid, 1.0);
+			//verts[newVertices[j]].light.z += adjustment;
 			verts[newVertices[j]].flags |= VERT_SHIFTED;
+			//verts[newVertices[j]].normal = original->faceNormals[tri->faceIndex];
 		}
 	}
 	printf("1 mesh subdivided. %d verts, %d tris, %d edges, %d indices\n", numVerts, numTris, numEdges, numIndices);
