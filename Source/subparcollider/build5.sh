88bee68d5d259aa79c0bb9b8f225f7eacb434e79
Espen Overaae <minthos@gmail.com>
2023-07-16 22:01:33 +0200

    added the lintcpp script to the build script so it's done automatically in the future

--- Source/subparcollider/build5.sh
+++ Source/subparcollider/build5.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+cat render5.cpp | ./lintcpp.py > lintedrender5.cpp
+
 # optimized build
 #g++ -O3 -c render5.cpp sdlwrapper.cpp -fpermissive -fPIC -lglfw -lGL -lGLEW -lm -lSDL2 && swiftc -O -import-objc-header subparcollider-Bridging-Header.h main.swift gptphysics.swift render5.o sdlwrapper.o -o main -L/usr/local/lib -I/usr/local/include -lGLEW -lglfw -lGL -lm -lSDL2
 
 # ./build5.sh && valgrind --track-origins=yes ./main
 # debug build
-g++ -g -c render5.cpp sdlwrapper.cpp -fpermissive -fPIC -lglfw -lGL -lGLEW -lm -lSDL2 && swiftc -g -D DEBUG -import-objc-header subparcollider-Bridging-Header.h main.swift gptphysics.swift render5.o sdlwrapper.o -o main -L/usr/local/lib -I/usr/local/include -lGLEW -lglfw -lGL -lm -lSDL2
+g++ -g -c lintedrender5.cpp sdlwrapper.cpp -fpermissive -fPIC -lglfw -lGL -lGLEW -lm -lSDL2 && swiftc -g -D DEBUG -import-objc-header subparcollider-Bridging-Header.h main.swift gptphysics.swift lintedrender5.o sdlwrapper.o -o main -L/usr/local/lib -I/usr/local/include -lGLEW -lglfw -lGL -lm -lSDL2
