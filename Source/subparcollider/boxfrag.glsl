da9a13e7897178cc5bc332c74ccafe81e8068962
Minthos <minthos@gmail.com>
2024-03-07 03:27:51 +0700

    it even renders a cube

--- Source/subparcollider/boxfrag.glsl
+++ Source/subparcollider/boxfrag.glsl
@@ -5,7 +5,7 @@ in vec3 normal;
 out vec4 fragColor;
 
 void main() {
-    float brightness = dot(normalize(normal), normalize(vec3(0, 0, 1)));
+    float brightness = 1.0;
     vec3 color = vec3(0.1, 0.5, 0.8) * brightness;
     fragColor = vec4(color, 1.0);
 }
