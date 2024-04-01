cd5dd71cb219ad01c294e38b57ef2663486913f5
Minthos <minthos@gmail.com>
2024-04-01 20:56:14 +0700

    added sandy beaches, not great

--- Source/subparcollider/terrainfrag.glsl
+++ Source/subparcollider/terrainfrag.glsl
@@ -17,9 +17,12 @@ void main() {
     vec3 grass = mix(vec3(0.15, 0.4, 0.15), vec3(1.0), min(1.0, max(0.0, (elevation - 2000.0) / 1000.0)));
     vec3 rock = mix(vec3(0.7, 0.5, 0.3), vec3(0.15), min(1.0, max(0.0, elevation / 2000.0)));
     vec3 color = mix(grass, rock, max(0.0, min(1.0, (-0.5 + 5 * inclination) - max(0.0, (elevation - 3000) / 2000.0) )));
+    vec3 sand = mix(vec3(194/255.0, 178/255.0, 128/255.0), color, min(1.0, max(0.0, (elevation + (20.0 * inclination)) / 10.0)));
     if(elevation < 1.0){
         color = vec3(0.1, 0.2, 0.3);
         fragColor = vec4(color * (3.0 + insolation) * 0.25, 1.0);
+    } else if(elevation < 100.0) {
+        fragColor = vec4(sand * insolation, 1.0);
     } else {
         fragColor = vec4(color * insolation, 1.0);
     }
